41c73a9115e3a9e6e18d6c2336598b56878cd061
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-11-26 12:50:28 -0600

    user created - may need editing

--- src/main/java/com/tracker/feeding/models/User.java
+++ src/main/java/com/tracker/feeding/models/User.java
@@ -1,4 +1,88 @@
 package com.tracker.feeding.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 25)
+    private String username;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String first_name;
+
+    @Column(nullable = false, length = 50)
+    private String last_name;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    public User(long id, String username, String email, String first_name, String last_name, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.password = password;
+    }
+
+    public User () {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
