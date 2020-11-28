7b50830f1639f03d8bb2130a45ca8b352dea53b6
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-11-28 14:48:29 -0600

    baby view - minimal

--- src/main/java/com/tracker/feeding/controllers/BabyController.java
+++ src/main/java/com/tracker/feeding/controllers/BabyController.java
@@ -5,18 +5,26 @@ import com.tracker.feeding.repositories.BabyRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BabyController {
     private final BabyRepository babyDao;
 
-    public BabyController(BabyController babyDao) {
+    public BabyController(BabyRepository babyDao) {
         this.babyDao = babyDao;
     }
 
-    @GetMapping("/baby")
+    @GetMapping("/mybaby")
     public String addBaby(Model model){
         model.addAttribute("baby", new Baby());
         return "babies/addbaby";
+    }
+
+    @PostMapping("/addbaby")
+    public String saveBaby(@ModelAttribute Baby baby) {
+        babyDao.save(baby);
+        return "redirect:/addbaby";
     }
 }
--- src/main/java/com/tracker/feeding/repositories/BabyRepository.java
+++ src/main/java/com/tracker/feeding/repositories/BabyRepository.java
@@ -4,5 +4,5 @@ import com.tracker.feeding.models.Baby;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BabyRepository extends JpaRepository<Baby, Long> {
-    Baby findByBabyname(String babyname);
+    Baby findByName(String name);
 }
