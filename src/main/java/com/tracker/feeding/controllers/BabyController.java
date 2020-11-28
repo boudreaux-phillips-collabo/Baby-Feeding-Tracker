6c18359d5f323d412ded758904b72c37fb780e72
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-11-28 14:11:46 -0600

    building baby views

--- src/main/java/com/tracker/feeding/controllers/BabyController.java
+++ src/main/java/com/tracker/feeding/controllers/BabyController.java
@@ -1,4 +1,22 @@
 package com.tracker.feeding.controllers;
 
+import com.tracker.feeding.models.Baby;
+import com.tracker.feeding.repositories.BabyRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BabyController {
+    private final BabyRepository babyDao;
+
+    public BabyController(BabyController babyDao) {
+        this.babyDao = babyDao;
+    }
+
+    @GetMapping("/baby")
+    public String addBaby(Model model){
+        model.addAttribute("baby", new Baby());
+        return "babies/addbaby";
+    }
 }
--- src/main/java/com/tracker/feeding/models/Baby.java
+++ src/main/java/com/tracker/feeding/models/Baby.java
@@ -1,4 +1,23 @@
 package com.tracker.feeding.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "baby_details")
 public class Baby {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 25)
+    private String name;
+
+    @DateTimeFormat
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date dob;
 }
