431346ffde0135e7ec35b7641610d717bb2fcbf3
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-11-28 21:33:14 -0600

    setting up authentication

--- src/main/java/com/tracker/feeding/SecurityConfiguration.java
+++ src/main/java/com/tracker/feeding/SecurityConfiguration.java
@@ -1,15 +1,57 @@
 package com.tracker.feeding;
 
-//import com.tracker.feeding.services.UserDetailsLoader;
-//import org.springframework.context.annotation.Bean;
+import com.tracker.feeding.services.UserDetailsLoader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration {
+    private UserDetailsLoader usersLoader;
 
+    public SecurityConfiguration(UserDetailsLoader usersLoader) { this.usersLoader = usersLoader; }
+
+    @Bean
+    public PasswordEncoder passwordencoder() { return new BCryptPasswordEncoder(); }
+
+    @Override
+    protected void configure(Authentication auth) throws Exception {
+        auth
+                .userDetailsService(usersLoader)
+                .passwordEncoder(passwordEncoder())
+            ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                /* Login configuration */
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/profile") // user's home page, it can be any URL
+                .permitAll() // Anyone can go to the login page
+                /* Logout configuration */
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login") // append a query string value ?logout
+                /* Pages that can be viewed without having to log in */
+                .and()
+                .authorizeRequests()
+                .antMatchers("/") // anyone can see the home page
+                .permitAll()
+                /* Pages that require authentication */
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/addbaby", // only authenticated users can create babies
+                        "/babies/edit/{id}" // only authenticated users can edit babies
+                )
+                .authenticated()
+        ;
+    }
 }
--- src/main/java/com/tracker/feeding/models/UserWithRoles.java
+++ src/main/java/com/tracker/feeding/models/UserWithRoles.java
@@ -1,4 +1,39 @@
 package com.tracker.feeding.models;
 
-public class UserWithRoles {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collections;
+
+public class UserWithRoles extends User implements UserDetails {
+    public UserWithRoles(User user) {
+        super(user);
+    }
+
+    @Override
+    public Collections<? extends GrantedAuthority> getAuthorities() {
+        String roles = "";
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
